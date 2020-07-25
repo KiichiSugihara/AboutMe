11219174592475d4251096eef5ccdbb66d2fd0d2
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-07-25 14:23:08 +0900

    feat : use data-binding

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -13,9 +13,13 @@ import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Aleks Haecky")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
 //        setContentView(R.layout.activity_main)
 //        doneButton にaddNickname()を追加 引数it (View)
         binding.doneButton.setOnClickListener {
@@ -30,7 +34,8 @@ class MainActivity : AppCompatActivity() {
     }
     private fun addNickname(view: View) {
         binding.apply {
-            nicknameText.text = nicknameEdit.text.toString()
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
             nicknameEdit.visibility = View.GONE
             doneButton.visibility = View.GONE
             nicknameText.visibility = View.VISIBLE
